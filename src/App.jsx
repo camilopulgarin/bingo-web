61eb05fedd33108ce2fdd2bdd5f471bc56abe299
Tolo0322 <tolo03221986@gmail.com>
2024-12-20 11:05:36 -0500

    arquitectura de pagina de registro

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import AppRoutes from './routes/Approutes';
 
-
 function App() {
   return <AppRoutes />;
 }
--- src/routes/Approutes.jsx
+++ src/routes/Approutes.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../pages/login';
-import Register from '../pages/register';
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        {/* Ruta para Login */ }
+        {/* Ruta para Login */}
         <Route path="/login" element={<Login />} />
 
         {/* Redireccionar a login por defecto */}
-        <Route path="*" element={<Navigate to="/login" replace />} /> 
-        <Route path="/register" element={<Register />} />
-
-        {/* Redireccionar a login por defecto */}
-        <Route path="*" element={<Navigate to="/register" replace />} /> 
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
   );
