77208fadfe157ca68bd8380dcfa27cdc3c4ad6ad
Tolo0322 <tolo03221986@gmail.com>
2025-01-16 21:00:10 -0500

    Terminos y condiciones

--- src/routes/Approutes.jsx
+++ src/routes/Approutes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../pages/login';
 import Register from '../pages/register';
+import TermsAndConditions from '../pages/TermsAndConditions/TermsAndConditions';
 
 const AppRoutes = () => {
   //comentario
@@ -14,6 +15,8 @@ const AppRoutes = () => {
         <Route path="/register" element={<Register />} />
         {/* Redireccionar a login por defecto */}
         <Route path="*" element={<Navigate to="/register" replace />} /> 
+        <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+        
       </Routes>
     </Router>
   );
