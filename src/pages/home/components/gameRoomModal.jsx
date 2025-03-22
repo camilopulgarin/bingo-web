ed620548398de79458ffa43b2a47a8196f1344ea
Andres Pulgarin <andres.pulgarin.nopia@gmail.com>
2025-03-21 20:08:49 -0500

    feat: ubdate dashboard style

--- src/pages/home/components/gameRoomModal.jsx
+++ src/pages/home/components/gameRoomModal.jsx
@@ -1,21 +1,32 @@
-import { Button, Box, Stack } from '@mui/material';
+import { Button, Box, Stack, TextField } from "@mui/material";
 
 const GameRoomModal = () => {
   return (
-    <Box sx={{ width: '320px',height: '320px', display: 'flex', flexDirection: 'column', alignItems:'center', justifyContent:'center', gap:'30px'}}>
-       <Stack sx={{ width: '200px' }}>
-            <Button  variant="contained" color="primary" size="large">
-            Unirse a una sala
-            </Button>
-       </Stack>
-       <Stack sx={{ width: '200px' }}>
-            <Button variant="contained" color="primary" size="large">
-                    Crear sala
-            </Button>
-       </Stack>
+    <Box
+      sx={{
+        width: "320px",
+        height: "320px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "30px",
+      }}
+    >
+      <Stack sx={{ width: "200px" }}>
+        <TextField label="Correo Electrónico" variant="outlined" />
+      </Stack>
+      <Stack sx={{ width: "200px" }}>
+        <Button variant="contained" color="primary" size="large">
+          Unirse a una sala
+        </Button>
+      </Stack>
+      <Stack sx={{ width: "200px" }}>
+        <Button variant="contained" color="primary" size="large">
+          Crear sala
+        </Button>
+      </Stack>
     </Box>
-    )
-}
+  );
+};
 export default GameRoomModal;
-  
- 
