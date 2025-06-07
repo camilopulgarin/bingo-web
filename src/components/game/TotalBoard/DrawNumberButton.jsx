eb64233f553c0d69558b7178295bcfb966334593
Andres Pulgarin <andres.pulgarin.nopia@gmail.com>
2025-06-07 16:38:27 -0500

    feat: update bingo total history style

--- src/components/game/TotalBoard/DrawNumberButton.jsx
+++ src/components/game/TotalBoard/DrawNumberButton.jsx
@@ -6,10 +6,18 @@ import { addDrawnNumber } from '../../../redux/slices/game/BingoTotalCellSlice';
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 
+const getBingoLetter = (number) => {
+  if (number <= 15) return 'B';
+  if (number <= 30) return 'I';
+  if (number <= 45) return 'N';
+  if (number <= 60) return 'G';
+  return 'O';
+};
+
 const DrawNumberButton = () => {
   const dispatch = useDispatch();
   const drawnNumbers = useSelector((state) => state.BingoTotalCell.drawnNumbers);
-  const [lastNumber, setLastNumber] = useState(null);
+  const [lastDrawn, setLastDrawn] = useState(null);
 
   const drawNumber = () => {
     const availableNumbers = Array.from({ length: 75 }, (_, i) => i + 1).filter(
@@ -23,9 +31,10 @@ const DrawNumberButton = () => {
 
     const randomIndex = Math.floor(Math.random() * availableNumbers.length);
     const number = availableNumbers[randomIndex];
+    const letter = getBingoLetter(number);
 
     dispatch(addDrawnNumber(number));
-    setLastNumber(number);
+    setLastDrawn({number, letter});
   };
 
   return (
@@ -33,16 +42,19 @@ const DrawNumberButton = () => {
       {/* Animación del último número */}
       <div className="relative h-32 w-32 flex items-center justify-center">
         <AnimatePresence>
-          {lastNumber && (
+          {lastDrawn && (
             <motion.div
-              key={lastNumber}
+              key={lastDrawn.number}
               initial={{ rotate: -180, opacity: 0 }}
               animate={{ rotate: 0, opacity: 1 }}
               exit={{ opacity: 0 }}
               transition={{ duration: 0.5, ease: 'easeInOut' }}
               className="absolute text-6xl text-yellow-800 font-retro bg-yellow-200 border-4 border-yellow-800 rounded-full h-24 w-24 flex items-center justify-center shadow-xl"
             >
-              {lastNumber}
+              <div className='flex flex-col items-center justify-center mb-2'>
+                <div className="text-5xl text-yellow-900 font-retro">{lastDrawn.letter}</div>
+                <div className="text-4xl font-bold text-yellow-900">{lastDrawn.number}</div>
+              </div>
             </motion.div>
           )}
         </AnimatePresence>
